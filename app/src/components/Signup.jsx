08abd7ba4a4da7fb5c8635c14c000aeb09396007
Pav0l <pavol.karas@gmail.com>
2019-03-19 16:30:50 +0100

    Populate Signup component, add styles

--- app/src/components/Signup.jsx
+++ app/src/components/Signup.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Form from './shared/Form';
+import SocialHeader from './shared/SocialHeader';
 
 export default class Signup extends React.Component {
 
   render () {
     return (
-      <div>
-        Signup
+      <SignupContainer>
+        <SocialHeader />
         <Form />
-      </div>
+      </SignupContainer>
     );
   }
 }
+
+const SignupContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 200px;
+`;
